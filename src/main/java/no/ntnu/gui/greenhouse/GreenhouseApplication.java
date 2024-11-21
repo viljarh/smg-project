d61e90cf8232730b7a23838f4f3f0b112ea03f93
gauteao <99391960+gauteao@users.noreply.github.com>
2024-11-21 22:28:03 +0100

    Fixed bad files

--- src/main/java/no/ntnu/gui/greenhouse/GreenhouseApplication.java
+++ src/main/java/no/ntnu/gui/greenhouse/GreenhouseApplication.java
@@ -14,66 +14,66 @@ import no.ntnu.tools.Logger;
  * Run a greenhouse simulation with a graphical user interface (GUI), with JavaFX.
  */
 public class GreenhouseApplication extends Application implements NodeStateListener {
-  private static GreenhouseSimulator simulator;
-  private final Map<SensorActuatorNode, NodeGuiWindow> nodeWindows = new HashMap<>();
-  private Stage mainStage;
+    private static GreenhouseSimulator simulator;
+    private final Map<SensorActuatorNode, NodeGuiWindow> nodeWindows = new HashMap<>();
+    private Stage mainStage;
 
-  @Override
-  public void start(Stage mainStage) {
-    Logger.info("GreenhouseApplication.start() called");
-    this.mainStage = mainStage;
-    Logger.info("Setting up the main stage");
+    @Override
+    public void start(Stage mainStage) {
+        Logger.info("GreenhouseApplication.start() called");
+        this.mainStage = mainStage;
+        Logger.info("Setting up the main stage");
 
-    mainStage.setScene(new MainGreenhouseGuiWindow());
-    mainStage.setMinWidth(MainGreenhouseGuiWindow.WIDTH);
-    mainStage.setMinHeight(MainGreenhouseGuiWindow.HEIGHT);
-    mainStage.setTitle("Greenhouse simulator");
-    mainStage.show();
-    Logger.info("Main stage displayed");
-    Logger.info("GUI subscribes to lifecycle events");
-    simulator.initialize();
-    simulator.subscribeToLifecycleUpdates(this);
-    mainStage.setOnCloseRequest(event -> closeApplication());
-    simulator.start();
-  }
-
-  private void closeApplication() {
-    Logger.info("Closing Greenhouse application...");
-    simulator.stop();
-    try {
-      stop();
-    } catch (Exception e) {
-      Logger.error("Could not stop the application: " + e.getMessage());
+        mainStage.setScene(new MainGreenhouseGuiWindow());
+        mainStage.setMinWidth(MainGreenhouseGuiWindow.WIDTH);
+        mainStage.setMinHeight(MainGreenhouseGuiWindow.HEIGHT);
+        mainStage.setTitle("Greenhouse simulator");
+        mainStage.show();
+        Logger.info("Main stage displayed");
+        Logger.info("GUI subscribes to lifecycle events");
+        simulator.initialize();
+        simulator.subscribeToLifecycleUpdates(this);
+        mainStage.setOnCloseRequest(event -> closeApplication());
+        simulator.start();
     }
-  }
 
-  /**
-   * Start the GUI Application.
-   *
-   * @param fake When true, emulate fake events instead of opening real sockets
-   */
-  public static void startApp(boolean fake) {
-    Logger.info("Running greenhouse simulator with JavaFX GUI...");
-    simulator = new GreenhouseSimulator(fake);
-    launch();
-  }
-
-  @Override
-  public void onNodeReady(SensorActuatorNode node) {
-    Logger.info("Starting window for node " + node.getId());
-    NodeGuiWindow window = new NodeGuiWindow(node);
-    nodeWindows.put(node, window);
-    window.show();
-  }
-
-  @Override
-  public void onNodeStopped(SensorActuatorNode node) {
-    NodeGuiWindow window = nodeWindows.remove(node);
-    if (window != null) {
-      Platform.runLater(window::close);
-      if (nodeWindows.isEmpty()) {
-        Platform.runLater(mainStage::close);
-      }
+    private void closeApplication() {
+        Logger.info("Closing Greenhouse application...");
+        simulator.stop();
+        try {
+            stop();
+        } catch (Exception e) {
+            Logger.error("Could not stop the application: " + e.getMessage());
+        }
     }
-  }
+
+    /**
+     * Start the GUI Application.
+     *
+     * @param fake When true, emulate fake events instead of opening real sockets
+     */
+    public static void startApp(boolean fake) {
+        Logger.info("Running greenhouse simulator with JavaFX GUI...");
+        simulator = new GreenhouseSimulator(fake);
+        launch();
+    }
+
+    @Override
+    public void onNodeReady(SensorActuatorNode node) {
+        Logger.info("Starting window for node " + node.getId());
+        NodeGuiWindow window = new NodeGuiWindow(node);
+        nodeWindows.put(node, window);
+        window.show();
+    }
+
+    @Override
+    public void onNodeStopped(SensorActuatorNode node) {
+        NodeGuiWindow window = nodeWindows.remove(node);
+        if (window != null) {
+            Platform.runLater(window::close);
+            if (nodeWindows.isEmpty()) {
+                Platform.runLater(mainStage::close);
+            }
+        }
+    }
 }
--- src/main/java/no/ntnu/run/GreenhouseGuiStarter.java
+++ src/main/java/no/ntnu/run/GreenhouseGuiStarter.java
@@ -7,21 +7,21 @@ import no.ntnu.tools.Logger;
  * Starter for GUI version of the greenhouse simulator.
  */
 public class GreenhouseGuiStarter {
-  /**
-   * Entrypoint gor the Greenhouse GUI application.
-   *
-   * @param args Command line arguments, only the first one of them used: when it is "fake",
-   *             emulate fake events, when it is either something else or not present,
-   *             use real socket communication.
-   */
-  public static void main(String[] args) {
-    Logger.info("Starting GreenhouseGuiStarter...");
-    boolean fake = false;
-    if (args.length == 1 && "fake".equals(args[0])) {
-        fake = true;
-        Logger.info("Using FAKE events");
+    /**
+     * Entrypoint gor the Greenhouse GUI application.
+     *
+     * @param args Command line arguments, only the first one of them used: when it is "fake",
+     *             emulate fake events, when it is either something else or not present,
+     *             use real socket communication.
+     */
+    public static void main(String[] args) {
+        Logger.info("Starting GreenhouseGuiStarter...");
+        boolean fake = false;
+        if (args.length == 1 && "fake".equals(args[0])) {
+            fake = true;
+            Logger.info("Using FAKE events");
+        }
+        Logger.info("Launching GreenhouseApplication...");
+        GreenhouseApplication.startApp(fake);
     }
-    Logger.info("Launching GreenhouseApplication...");
-    GreenhouseApplication.startApp(fake);
-}
 }
